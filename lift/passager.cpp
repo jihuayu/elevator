93dd93c108bed04fd9bc58e7b446fd4c23a7a672
jihuayu <8042833@qq.com>
2018-02-07 14:30:20 +0800

    feat:使用的新的写法。基本实现了智能选择。

---
+++ lift/passager.cpp
@@ -0,0 +1,63 @@
+#include "stdafx.h"
+#include "passager.h"
+
+
+passager::passager()
+{
+  static int i = 0;
+  id = i;
+  isok = 0;
+  goal = 0;
+  i++;
+}
+
+
+passager::~passager()
+{
+}
+int passager::in_out(int liftfloor,int nowtime) {
+  int costtime=0;
+  if (goal == liftfloor) {
+    if (isok == 0) {
+      isok = 1;
+      goal = direction;
+      costtime = -1;
+    }
+  }
+  if (goal == liftfloor) {
+    if (isok == 1) {
+      isok = 2;
+      goal = 0;
+      costtime = nowtime - time;
+    }
+  }
+  return costtime;
+}
+
+int passager::call(int nowtime) {
+  if (nowtime >= time&&isok==0) {
+    
+    goal=floor;
+  }
+  else if(nowtime >= time && isok == 1){
+    goal = direction;
+  }
+  else {
+    goal = 0;
+  }
+  return goal;
+}
+
+istream & operator >>(istream & is, passager & c) {
+  is >> c.time >> c.floor;
+  int num;
+  is >> num;
+  if (num == 0) {
+    c.direction = 10;
+  }
+  else {
+    c.direction = 1;
+  }
+  c.goal = 0;
+  return is;
+}
---
+++ lift/passager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class passager
+{
+public:
+  int id;
+  int isok;
+  int floor;
+  int direction;
+  int time;
+  int goal;
+
+  passager();
+  ~passager();
+
+  int call(int);
+  int in_out(int,int);
+  friend istream & operator >>(istream &, passager &);
+};
+
